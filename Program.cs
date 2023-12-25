bfaf00808d071eb48a5cba760f571ee070c19e9c
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-25 21:35:05 +0400

    secTask

--- Program.cs
+++ Program.cs
@@ -4,19 +4,42 @@
 // Примеры
 // a = 256 => 26
 // a = 891 => 81
-Console.WriteLine("Введдите число");
-int num= Convert.ToInt32(Console.ReadLine());
-// int num2= int.Parse(Console.ReadLine());
+// Console.WriteLine("Введдите число");
+// int num= Convert.ToInt32(Console.ReadLine());
+// // int num2= int.Parse(Console.ReadLine());
 
-if(num>99 && num<1000)     //    && -и     || - или
-{
-    int num1 = num/100;
-    int num3 = num % 10;
+// if(num>99 && num<1000)     //    && -и     || - или
+// {
+//     int num1 = num/100;
+//     int num3 = num % 10;
 
-    int result = num1*10+num3;
+//     int result = num1*10+num3;
 
-    Console.WriteLine(result);
-}
+//     Console.WriteLine(result);
+// }
+
+// sectask
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
 
 
+// int num = new Random().Next(100,1000);
+// Console.WriteLine(num);
+
+// int num2 = num/10 %10;
+// int num3 = num % 10;
+// int result = 1;
+
+// for (int i = 0; i < num3; i++)
+// {
+//     result = result * num2;
+// }
+// System.Console.WriteLine(result);
+
+//зад3
 
